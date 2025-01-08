2675dfe1711192707ddd06fef14add5b2ca20c2e
Cornelius-G <cornelius.grunwald@tu-dortmund.de>
2025-01-08 15:54:03 +0100

    setup docs

---
+++ docs/make.jl
@@ -0,0 +1,32 @@
+# Use
+#
+#     DOCUMENTER_DEBUG=true julia --color=yes make.jl local [nonstrict] [fixdoctests]
+#
+# for local builds.
+
+using Documenter
+using MCBench
+
+makedocs(
+    sitename = "MCBench",
+    modules = [MCBench],
+    format = Documenter.HTML(
+        prettyurls = !("local" in ARGS),
+        canonical = "https://github.com/tudo-physik-e4/MCBench"
+    ),
+    pages = [
+        "Home" => "index.md",
+        "API" => "api.md",
+        "LICENSE" => "LICENSE.md",
+    ],
+    doctest = ("fixdoctests" in ARGS) ? :fix : true,
+    linkcheck = ("linkcheck" in ARGS),
+    warnonly = !("nonstrict" in ARGS),
+)
+
+deploydocs(
+    repo = "github.com/tudo-physik-e4/MCBench.git",
+    forcepush = true,
+    push_preview = true,
+    devbranch = "main"
+)
---
+++ docs/src/api.md
@@ -0,0 +1,26 @@
+# API
+
+```@meta
+DocTestSetup  = quote
+    using MCBench
+end
+```
+
+## Types
+
+```@index
+Order = [:type, :constant]
+```
+
+## Functions
+
+```@index
+Order = [:macro, :function]
+```
+
+# Documentation
+
+```@autodocs
+Modules = [MCBench]
+Order = [:type, :constant, :macro, :function]
+```
