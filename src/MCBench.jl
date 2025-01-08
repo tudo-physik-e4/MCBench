a2f9b7d17d237fd290a9a0a52c15f5d00ac4b730
Cornelius-G <cornelius.grunwald@tu-dortmund.de>
2025-01-08 14:32:30 +0100

    Modify Package structure, add Project.toml

---
+++ src/MCBench.jl
@@ -0,0 +1,32 @@
+using Distributions 
+using Statistics 
+using ValueShapes
+using LinearAlgebra
+using Folds
+using IntervalSets
+using BAT
+using StatsBase
+using Plots
+using JSON  #for pasting teststatistic quickly 
+using StructArrays
+using HypothesisTests
+using DensityInterface
+import DensityInterface: logdensityof
+using IPMeasures
+using Distances 
+using DataFrames
+using CSV
+
+include("samplers.jl")
+include("testcases.jl")
+include("mmd.jl")
+include("testmetrics.jl")
+include("wasserstein.jl")
+include("twosampleteststatics.jl")
+include("marginal_utils.jl")
+include("teststatistic.jl")
+include("sample_utils.jl")
+include("plotting_teststat.jl")
+include("batmh.jl")
+include("../examples/example_distributions.jl")
+include("../examples/example_posteriordb.jl")
